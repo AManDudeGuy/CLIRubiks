74ef0a5c2803006e473f49aa8dd2b8ace8cfb066
Ali <syedaliabbask37@gmail.com>
2026-02-13 18:26:20 +0400

    Terminal printing testing

---
+++ src/cube.cpp
@@ -0,0 +1,15 @@
+class Cube {
+    private:
+        int size;
+        void initCube(){
+        }
+    public:
+        Cube(){
+            int size = 3;
+            initCube();
+        }
+
+        void cubePrint(){
+
+        }
+};
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+int main(){
+    const std::string RESET = "\033[0m";
+    const std::string RED = "\033[31m";
+    const std::string GREEN = "\033[32m";
+    const std::string YELLOW = "\033[33m";
+    const std::string BLUE = "\033[34m";
+    const std::string WHITE = "\033[37m";
+    const std::string ORANGE = "\033[35m";
+    std::cout << RED << "Hi" << RESET;
+    return 0;
+}
